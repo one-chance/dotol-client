5d01e9bd8004bcde4a0eafeb11dddb79190a3f0e
one-chance <woody.front@gmail.com>
2023-06-26 20:09:18 +0900

    feat: Change main router format

--- src/routes/MainRouter.tsx
+++ src/routes/MainRouter.tsx
@@ -1,7 +1,7 @@
 import { useWindowSize } from '@utils/hooks';
 import { Route, Routes } from 'react-router-dom';
 
-import * as MainRoutes from './MainRoutes';
+import MainRoutes from './MainRoutes';
 
 export default () => {
   const { width } = useWindowSize();
@@ -14,11 +14,7 @@ export default () => {
         minHeight: width > 1080 ? `calc(100vh - 148px)` : `calc(100vh - 108px)`,
       }}
     >
-      <Routes>
-        {Object.entries(MainRoutes).map(([key, value]) => (
-          <Route key={key} {...value} />
-        ))}
-      </Routes>
+      <MainRoutes />
     </main>
   );
 };
--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -15,187 +15,81 @@ import { Home, NoMatch, PrivacyPolicy, TermsOfService } from '@pages/index';
 import { Auction, TradeBoard } from '@pages/trade';
 import {
   ChangePassword,
+  CharacterList,
   FindAccount,
   Profile,
   Secession,
   SignIn,
   SignUp,
 } from '@pages/user';
+import { isLoggedInState } from '@states/login';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
 
-/* 게시판 메뉴 */
-export const FreeBoardPage = {
-  path: `/board/free`,
-  element: <FreeBoard />,
-};
+export default () => {
+  const isLoggedIn = useRecoilValue(isLoggedInState);
 
-export const ServerBoardPage = {
-  path: `/board/server`,
-  element: <ServerBoard />,
-};
+  return (
+    <Routes>
+      {/* 사용자 메뉴 */}
+      <Route
+        element={isLoggedIn ? <Profile /> : <Navigate to="/" />}
+        path="/user/profile"
+      />
+      <Route element={<CharacterList />} path="/user/character" />
+      <Route
+        element={isLoggedIn ? <ChangePassword /> : <Navigate to="/" />}
+        path="/user/change-password"
+      />
+      <Route
+        element={isLoggedIn ? <Secession /> : <Navigate to="/" />}
+        path="/user/secession"
+      />
+      <Route element={<FindAccount />} path="/user/find-account" />
+      <Route element={<SignIn />} path="/user/signin" />
+      <Route element={<SignUp />} path="/user/signup" />
 
-export const TipBoardPage = {
-  path: `/board/tip`,
-  element: <TipBoard />,
-};
+      {/* 게시판 메뉴 */}
+      <Route element={<FreeBoard />} path="/board/free" />
+      <Route element={<TipBoard />} path="/board/tip" />
+      <Route element={<VideoBoard />} path="/board/video" />
+      <Route element={<TradeBoard />} path="/board/trade" />
+      <Route element={<ServerBoard />} path="/board/server" />
+      <Route element={<Post />} path="/board/:boardId/post/:postId" />
+      <Route element={<WritePost />} path="/board/:boardId/write" />
+      <Route element={<EditPost />} path="/board/:boardId/edit/:postId" />
 
-export const VideoBoardPage = {
-  path: `/board/video`,
-  element: <VideoBoard />,
-};
+      {/* 코디 메뉴 */}
+      <Route element={<LuxurySeries />} path="/costume/list/luxury" />
+      <Route element={<Costumes />} path="/costume/list/*" />
+      <Route element={<Lookbook />} path="/costume/lookbook" />
+      <Route element={<Tanning />} path="/costume/tanning" />
 
-export const TradeBoardPage = {
-  path: `/board/trade`,
-  element: <TradeBoard />,
-};
+      {/* 도감 메뉴 */}
+      <Route element={<AntiquityEquip />} path="/db/antiquity-equip" />
+      <Route element={<NormalEquip />} path="/db/normal-equip" />
+      <Route element={<PetEquip />} path="/db/pet-equip" />
+      <Route element={<SkillAbility />} path="/db/skill-ability" />
 
-export const PostPage = {
-  path: `/board/:category/post/:seq`,
-  element: <Post />,
-};
+      {/* 콘텐츠 메뉴 */}
+      <Route element={<Achievement />} path="/content/achievement" />
+      <Route element={<Adventure />} path="/content/adventure" />
+      <Route element={<Archeology />} path="/content/archeology" />
 
-export const WritePostPage = {
-  path: `/board/:cateogry/write`,
-  element: <WritePost />,
-};
+      {/* 계산기 메뉴 */}
+      <Route element={<Ability />} path="/calculator/ability" />
+      <Route element={<Calendar />} path="/calculator/calendar" />
+      <Route element={<Power />} path="/calculator/power" />
+      <Route element={<Production />} path="/calculator/production" />
 
-export const EditPostPage = {
-  path: `/board/:cateogry/edit/:seq`,
-  element: <EditPost />,
-};
+      {/* 거래소 메뉴 */}
+      <Route element={<Auction />} path="/trade/auction" />
 
-/* 계산기 메뉴 */
-export const AbilityPage = {
-  path: `/calculator/ability`,
-  element: <Ability />,
-};
-
-export const CalendarPage = {
-  path: `/calculator/calendar`,
-  element: <Calendar />,
-};
-
-export const PowerPage = {
-  path: `/calculator/power`,
-  element: <Power />,
-};
-
-export const ProductionPage = {
-  path: `/calculator/production`,
-  element: <Production />,
-};
-
-/* 코디 메뉴 */
-export const LuxurySeriesPage = {
-  path: `/costume/list/luxury`,
-  element: <LuxurySeries />,
-};
-
-export const CostumesPage = {
-  path: `/costume/list/*`,
-  element: <Costumes />,
-};
-
-export const LookbookPage = {
-  path: `/costume/lookbook`,
-  element: <Lookbook />,
-};
-
-export const TanningPage = {
-  path: `/costume/tanning`,
-  element: <Tanning />,
-};
-
-/* 도감 메뉴 */
-export const AntiquityEquipPage = {
-  path: `/db/antiquity-equip`,
-  element: <AntiquityEquip />,
-};
-
-export const NormalEquipPage = {
-  path: `/db/normal-equip`,
-  element: <NormalEquip />,
-};
-
-export const PetEquipPage = {
-  path: `/db/pet-equip`,
-  element: <PetEquip />,
-};
-
-export const SkillAbilityPage = {
-  path: `/db/skill-ability`,
-  element: <SkillAbility />,
-};
-
-/* 컨텐츠 메뉴 */
-export const AchievementPage = {
-  path: `/content/achievement`,
-  element: <Achievement />,
-};
-
-export const AdventurePage = {
-  path: `/content/adventure`,
-  element: <Adventure />,
-};
-
-export const ArcheologyPage = {
-  path: `/content/archeology`,
-  element: <Archeology />,
-};
-
-/* 거래소 메뉴 */
-export const AuctionPage = {
-  path: `/trade/auction`,
-  element: <Auction />,
-};
-
-/* 사용자 메뉴 */
-export const ProfilePage = {
-  path: `/user/profile`,
-  element: <Profile />,
-};
-
-export const FindAccountPage = {
-  path: `/user/find-account`,
-  element: <FindAccount />,
-};
-
-export const SignInPage = {
-  path: `/user/signin`,
-  element: <SignIn />,
-};
-
-export const SignUpPage = {
-  path: `/user/signup`,
-  element: <SignUp />,
-};
-
-export const SecessionPage = {
-  path: `/user/secession`,
-  element: <Secession />,
-};
-
-export const ChangePasswordPage = {
-  path: `/user/change-password`,
-  element: <ChangePassword />,
-};
-
-/* 기본 페이지 */
-export const HomePge = {
-  path: `/`,
-  element: <Home />,
-};
-
-export const PrivacyPolicyPage = {
-  path: `/privacy-policy`,
-  element: <PrivacyPolicy />,
-};
-
-export const TermsOfServicePage = {
-  path: `/terms-of-service`,
-  element: <TermsOfService />,
-};
-
-export const NoMatchPage = {
-  path: `/*`,
-  element: <NoMatch />,
+      {/* 기본 메뉴 */}
+      <Route element={<Home />} path="/" />
+      <Route element={<PrivacyPolicy />} path="/privacy-policy" />
+      <Route element={<TermsOfService />} path="/terms-of-service" />
+      <Route element={<NoMatch />} path="*" />
+    </Routes>
+  );
 };
