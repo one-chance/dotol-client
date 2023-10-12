313fb3ba013f16820ffeab52ea6dfda82920fba6
one-chance <woody.front@gmail.com>
2023-10-12 12:43:07 +0900

    feat: Update october clothes

--- src/components/home-page/ClothSection.tsx
+++ src/components/home-page/ClothSection.tsx
@@ -24,17 +24,17 @@ export default () => {
       <FlexView gap={10} fill>
         <FlexView fill>
           <Text small={!isMobile} xSmall={isMobile} semiBold>
-            멋진의상재봉틀 - 야옹집사 의상
+            멋진의상재봉틀 - 풍월요괴고 의상
           </Text>
           <FlexView content="center" items="end" fill row wrap>
-            <Image alt="이미지1" height={150} src="/1.gif" width={150} />
-            <Image alt="이미지2" height={150} src="/2.gif" width={150} />
+            <Image alt="이미지1" height={160} src="/1.png" width={120} />
+            <Image alt="이미지2" height={160} src="/2.png" width={120} />
           </FlexView>
         </FlexView>
 
         <FlexView fill>
           <Text semiBold small>
-            의상수선가위 - 향긋한정원 의상
+            의상수선가위 - 고스트헌터 의상
           </Text>
           <FlexView center fill row wrap>
             <Image alt="이미지3" height={180} src="/3.gif" width={180} />
