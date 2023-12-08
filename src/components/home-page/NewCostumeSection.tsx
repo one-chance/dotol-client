09b0994792b3806c767bcf6495a46b7b7dcd77df
one-chance <woody.front@gmail.com>
2023-12-08 13:57:22 +0900

    feat: Add new costumes for december

--- src/components/home-page/NewCostumeSection.tsx
+++ src/components/home-page/NewCostumeSection.tsx
@@ -14,38 +14,28 @@ export default function NewCostumeSection() {
     >
       <FlexView content="between" row>
         <Text size={isMobile ? `small` : `normal`} weight="bold">
-          11월 신규 치장
+          12월 신규 치장
         </Text>
       </FlexView>
 
       <FlexView gap={10} fill>
         <FlexView fill>
           <Text size={isMobile ? `xSmall` : `small`} weight="semiBold">
-            멋진의상재봉틀 - 레서판다 의상
+            멋진의상재봉틀 - 우아한눈꽃 의상
           </Text>
           <FlexView content="center" items="end" fill row wrap>
-            <Image alt="이미지1" height={140} src="/1.gif" width={190} />
-            <Image alt="이미지2" height={140} src="/2.gif" width={190} />
+            <Image alt="이미지1" height={130} src="/1.png" width={120} />
+            <Image alt="이미지2" height={130} src="/2.png" width={120} />
           </FlexView>
         </FlexView>
 
         <FlexView fill>
           <Text size="small" weight="semiBold">
-            의상수선가위 - 주말오후 의상
+            의상수선가위 - 은하수샛별 의상
           </Text>
           <FlexView center fill row wrap>
-            <Image alt="이미지3" height={160} src="/3.gif" width={280} />
-            <Image alt="이미지4" height={160} src="/4.gif" width={280} />
-          </FlexView>
-        </FlexView>
-
-        <FlexView fill>
-          <Text size="small" weight="semiBold">
-            의상수선가위 - 스마트/엘리트교복 & 귀여운판다 의상
-          </Text>
-          <FlexView center fill row wrap>
-            <Image alt="이미지5" height={170} src="/5.gif" width={170} />
-            <Image alt="이미지6" height={170} src="/6.gif" width={170} />
+            <Image alt="이미지3" height={120} src="/3.png" width={120} />
+            <Image alt="이미지4" height={120} src="/4.png" width={120} />
           </FlexView>
         </FlexView>
       </FlexView>
