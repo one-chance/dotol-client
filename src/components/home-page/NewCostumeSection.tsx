2347f4866ab2b0e3a677fa757c051e2ea7fc439d
one-chance <woody.front@gmail.com>
2024-02-08 12:30:08 +0900

    feat: Update feburary costumes

--- src/components/home-page/NewCostumeSection.tsx
+++ src/components/home-page/NewCostumeSection.tsx
@@ -14,27 +14,32 @@ export default function NewCostumeSection() {
     >
       <FlexView content="between" row>
         <Text size={isMobile ? `small` : `normal`} weight="bold">
-          1월 신규 치장
+          2월 신규 치장
         </Text>
       </FlexView>
 
       <FlexView gap={10} fill>
         <FlexView fill>
           <Text size={isMobile ? `xSmall` : `small`} weight="semiBold">
-            멋진의상재봉틀 - 백악산군 의상
+            멋진의상재봉틀 - 인왕산 의상
           </Text>
           <FlexView content="center" items="end" fill row wrap>
-            <Image alt="이미지1" height={160} src="/1.gif" width={250} />
-            <Image alt="이미지2" height={135} src="/2.gif" width={250} />
+            <Image alt="이미지1" height={150} src="/1.png" width={140} />
+            <Image alt="이미지2" height={150} src="/2.png" width={140} />
           </FlexView>
         </FlexView>
 
         <FlexView fill>
           <Text size="small" weight="semiBold">
-            의상수선가위 - 명문교복 의상
+            의상수선가위 - 무공학관 & 천상계 의상
           </Text>
           <FlexView center fill row wrap>
-            <Image alt="이미지3" height={98} src="/3.png" width={218} />
+            <Image alt="이미지3" height={130} src="/3.gif" width={130} />
+            <Image alt="이미지4" height={130} src="/4.gif" width={130} />
+          </FlexView>
+          <FlexView center fill row wrap>
+            <Image alt="이미지5" height={130} src="/5.gif" width={130} />
+            <Image alt="이미지6" height={130} src="/6.gif" width={130} />
           </FlexView>
         </FlexView>
       </FlexView>
